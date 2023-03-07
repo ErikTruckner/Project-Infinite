e14d851b06e73d0c2a15fabfaf61100dd42a0d9b
Erik Truckner <eriktruckner@comcast.net>
2023-03-07 09:37:38 -0500

    limbo added - attached obstacles, rigid bodies, to tiles

--- src/Level.jsx
+++ src/Level.jsx
@@ -36,10 +36,66 @@ const BlockSpinner = ({ position = [0, 0, 0] }) => {
   // ***ANIMATION TIMER***
   useFrame((state) => {
     const time = state.clock.getElapsedTime()
+    // *** THIS IS ROTATION SPEED ***
 
     const rotation = new THREE.Quaternion()
     rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
     obstacle.current.setNextKinematicRotation(rotation)
+    // *** THIS ANCHORS OBSTABLE TO FLOOR TILE AND RIGID BODY***
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + 0.3,
+      z: position[2],
+    })
+  })
+
+  return (
+    <group position={position}>
+      {/* SECOND FLOOR TILE */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor2Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+      {/* OBSTACLE */}
+      <RigidBody
+        ref={obstacle}
+        type='kinematicPosition'
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}>
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          // position={[0, -0.1, 0]}
+          scale={[3.5, 0.3, 0.4]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  )
+}
+
+const BlockLimbo = ({ position = [0, 0, 0] }) => {
+  const obstacle = useRef()
+  // ***START TIME STAGGER FOR OBSTACLE***
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+  // ***ANIMATION TIMER***
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    // ***UP AND DOWN MOVEMENT WITH OFF SET***
+    const y = Math.sin(time + timeOffset) + 1.2
+
+    // *** THIS ANCHORS OBSTABLE TO FLOOR TILE AND RIGID BODY***
+
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + y,
+      z: position[2],
+    })
   })
 
   return (
@@ -82,10 +138,10 @@ const Level = () => {
 
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
+      <BlockStart position={[0, 0, 8]} />
       <BlockSpinner position={[0, 0, 0]} />
-      <BlockSpinner position={[0, 0, -4]} />
       <BlockSpinner position={[0, 0, -8]} />
+      <BlockLimbo position={[0, 0, -16]} />
     </>
   )
 }
