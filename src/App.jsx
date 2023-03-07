fd95e6cf748e322544df1cbbbad2b3fffe291c42
Erik Truckner <eriktruckner@comcast.net>
2023-03-07 13:20:13 -0500

    git test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug } from '@react-three/rapier'
-import Lights from './Lights'
 import Level from './Level'
 
 export default function App() {
@@ -15,13 +14,9 @@ export default function App() {
         <OrbitControls />
         <Physics>
           <Debug />
-<<<<<<< HEAD
-          <spotLight castShadow position={[5, 5, -14]} intensity={0.8} />
-          {/* <spotLight castShadow position={[5, 5, 14]} intensity={0.8} /> */}
+          <spotLight castShadow position={[5, 5, 14]} intensity={0.8} />
+          {/* <spotLight castShadow position={[5, 5, -14]} intensity={0.8} /> */}
 
-=======
-          <Lights />
->>>>>>> origin/main
           <Level />
         </Physics>
       </Canvas>
